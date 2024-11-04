f0c4e0fd18e77e14f07a9b7492ad91bf7ff48478
Ali Hemmat <AliHemmat.dev@gmail.com>
2024-11-04 11:43:41 +0330

    fix: change favicon

--- epaal/src/components/layout/Layout.jsx
+++ epaal/src/components/layout/Layout.jsx
@@ -1,24 +1,20 @@
-
 // components
 import Footer from "./Footeter";
 import Header from "./Header";
 
-
-
 export default function Layout({ children }) {
-
-    return (
-        <>
-            <Header />
-                <div
-                    className="
+  return (
+    <>
+      <Header />
+      <div
+        className="
                         w-[390px]
                         mx-auto
                     "
-                >
-                    { children }
-                </div>
-            <Footer />
-        </>
-    )
+      >
+        {children}
+      </div>
+      <Footer />
+    </>
+  );
 }
