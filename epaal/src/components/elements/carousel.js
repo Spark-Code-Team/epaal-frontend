5f35beb79ccc07c1572682607084b5c6bf620d22
Ali Hemmat <AliHemmat.dev@gmail.com>
2024-11-04 16:43:26 +0330

    build: carousel and search bar build & search field implemented\.

---
+++ epaal/src/components/elements/carousel.js
@@ -0,0 +1,107 @@
+"use client";
+
+import test1 from "@/../public/test/test(1).jpg";
+import test2 from "@/../public/test/test(2).jpg";
+import test3 from "@/../public/test/test(3).jpg";
+import { useState, useCallback } from "react";
+import Image from "next/image";
+import SwipeReact from "swipe-react";
+export default function Carousel() {
+  const images = [test1, test2, test3];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = useCallback(() => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  }, [images.length]);
+
+  const prevSlide = useCallback(() => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  }, [images.length]);
+
+  SwipeReact.config({
+    left: prevSlide,
+    right: nextSlide,
+  });
+  return (
+    <div className="relative w-full sm:max-w-sm md:max-w-md lg:max-w-lg">
+      {/* Image part */}
+      <div className="overflow-hidden" {...SwipeReact.events}>
+        <div
+          className="flex transition-transform duration-300 ease-in-out"
+          style={{ transform: `translateX(${currentIndex * 100}%)` }}
+        >
+          {images.map((src, index) => (
+            <div key={index} className="w-full flex-shrink-0">
+              <Image
+                src={src}
+                alt={`Carousel image ${index + 1}`}
+                sizes="100"
+                className="object-cover w-full h-full"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* slide next */}
+      <button
+        onClick={nextSlide}
+        className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-white/80 text-gray-800 p-2 rounded-md shadow hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
+        aria-label="Previous slide"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          className="w-2 h-2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M15 19l-7-7 7-7"
+          />
+        </svg>
+      </button>
+
+      <button
+        onClick={prevSlide}
+        className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-white/80 text-gray-800 p-2 rounded-md shadow hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
+        aria-label="Next slide"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          className="w-2 h-2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </button>
+
+      <div className="absolute bottom-2 left-0 right-0">
+        <div className="flex items-center justify-center gap-2">
+          {images.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => setCurrentIndex(index)}
+              className={`w-2 h-2 rounded-full ${
+                index === currentIndex ? "bg-white" : "bg-white/50"
+              }`}
+              aria-label={`Go to slide ${index + 1}`}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ epaal/src/components/elements/search.js
@@ -0,0 +1,14 @@
+function Search() {
+  return (
+    <div className="container p-2">
+      <input
+        name="searchInput"
+        className="p-4 bg-slate-50 w-full rounded-md"
+        type="text"
+        placeholder={"جستجو..."}
+      />
+    </div>
+  );
+}
+
+export default Search;
