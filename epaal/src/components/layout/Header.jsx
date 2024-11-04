f397f395517954738164b69a38fefa40fe0ced7e
Ali Hemmat <AliHemmat.dev@gmail.com>
2024-11-04 10:34:13 +0330

    fix: fix missing alt texts and dependencies

--- epaal/src/components/layout/Header.jsx
+++ epaal/src/components/layout/Header.jsx
@@ -1,69 +1,66 @@
 import Image from "next/image";
 import BurgerMenu from "../module/layoutModule/BurgerMenu";
 
-
-import logo from '../../../public/image/evaam-logo.png'
-
-
+import logo from "../../../public/image/evaam-logo.png";
 
 export default function Header() {
-
-    return (
-        <div
-            className="
+  return (
+    <div
+      className="
                 flex
                 flex-col
                 w-[390px]
                 bg-[#2852E4]
                 mx-auto
             "
-        >
-            <div
-                className="
+    >
+      <div
+        className="
                     flex
                     w-full
                     items-center
                     justify-between
                 "
-            >
-                <div
-                    className="
+      >
+        <div
+          className="
                         relative
                         flex
                         items-center
                         w-1/4
                         justify-evenly
                     "
-                >
-                    <BurgerMenu />
-                    <Image 
-                        src={logo}
-                        width={200}
-                        height={200}
-                        className="
+        >
+          <BurgerMenu />
+          <Image
+            src={logo}
+            width={200}
+            height={200}
+            className="
                             w-8
                             h-w-8
                         "
-                    />
-                </div>
-                <div
-                    className="
+            alt="evaam logo"
+          />
+        </div>
+        <div
+          className="
                         p-[12px]
                     "
-                >
-                    <div
-                        className="
+        >
+          <div
+            className="
                             text-xs
                             px-[18px]
                             py-[11.5px]
                             bg-white
                             rounded-lg 
                         "
-                    >
-                        ورود به حساب کاربری
-                    </div>
-                </div>
-            </div>
+          >
+            ورود به حساب کاربری
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- epaal/src/components/module/layoutModule/BurgerMenu.jsx
+++ epaal/src/components/module/layoutModule/BurgerMenu.jsx
@@ -1,62 +1,60 @@
-"use client"
+"use client";
 
 // Nextjs
-import Image from "next/image"
+import Image from "next/image";
 
 // IMG
-import burger from '../../../../public/icons/burgerMenu.svg'
+import burger from "../../../../public/icons/burgerMenu.svg";
 
 // React
-import { useState } from "react"
-
+import { useState } from "react";
 
 export default function BurgerMenu() {
+  const [open, setOpen] = useState(false);
 
-    const [open, setOpen] = useState(false)
-
-    return (
-        <>        
-            <Image 
-                src={burger}
-                width={24}
-                height={24}
-                className="
+  return (
+    <>
+      <Image
+        src={burger}
+        width={24}
+        height={24}
+        className="
                     w-6
                     h-6
                     cursor-pointer
                 "
-                onClick={() => setOpen(true)}
-            />
-            <div
-                className={`
+        alt="burger menu logo"
+        onClick={() => setOpen(true)}
+      />
+      <div
+        className={`
                     fixed
                     top-0
                     right-0
                     w-screen
                     h-screen
                     transform
-                    ${open ? 'translate-x-0' : 'translate-x-full'}
+                    ${open ? "translate-x-0" : "translate-x-full"}
                     transition-transform
                     bg-opacity-40
                     bg-slate-300
                     duration-500 
                     ease-in-out 
                 `}
-
-                onClick={() => setOpen(false)}
-            >
-                <div
-                    className="
+        onClick={() => setOpen(false)}
+      >
+        <div
+          className="
                         fixed
                         top-0 
                         right-0
                         h-full
                         bg-[#1D434C]
                     "
-                >
-dsojgvsp
-                </div>
-            </div>
-        </>
-    )
+        >
+          dsojgvsp
+        </div>
+      </div>
+    </>
+  );
 }
