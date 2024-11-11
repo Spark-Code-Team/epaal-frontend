a5a7210481243917e85a06fbd776b694e67e2363
Ali Hemmat <AliHemmat.dev@gmail.com>
2024-11-11 14:21:49 +0330

    BREAKING CHANGES: build: <ProductCard> component add: dana fonts fix: prettier config

--- epaal/src/app/layout.js
+++ epaal/src/app/layout.js
@@ -1,29 +1,28 @@
-
 // font
-import { Vazirmatn } from "next/font/google"
-
+import localFont from "next/font/local";
 // css
 import "./globals.css";
 import Layout from "@/components/layout/Layout";
-
 export const metadata = {
   title: "EPAAL",
   description: "Epaal",
 };
 
-const vazirmatn = Vazirmatn({ subsets: ['latin'] })
+const danaFont = localFont({
+  src: [
+    { path: "../../public/fonts/dana.ttf" },
+    { path: "../../public/fonts/dana.woff" },
+    { path: "../../public/fonts/dana.woff2" },
+  ],
+  display: "swap",
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html 
-      lang="fa"
-      dir="rtl"
-    >
-      <body
-        className={vazirmatn.className}
-      >
+    <html lang="fa" dir="rtl">
+      <body className={`${danaFont.className} bg-slate-100`}>
         {/* <Layout> */}
-          {children}
+        {children}
         {/* </Layout> */}
       </body>
     </html>
--- epaal/src/components/template/ShopPage.js
+++ epaal/src/components/template/ShopPage.js
@@ -9,6 +9,7 @@ import CategoryCard from "../elements/categoryCard";
 import mobilesCategory from "@/../public/image/mobilesCategory.png";
 import digitalsCategory from "@/../public/image/digitalsCategory.png";
 import homeCategory from "@/../public/image/homeCategory.png";
+import ProductCard from "../elements/productCard";
 
 export default function shop() {
   const images = [test1, test2, test3, test4, test5];
@@ -18,7 +19,7 @@ export default function shop() {
       <div className="container">
         <Carousel images={images} />
       </div>
-      <div className="container grid grid-cols-3 gap-2 p-2 mt-2">
+      <div className="container grid grid-cols-3 gap-2 p-2 my-4">
         <CategoryCard image={mobilesCategory} title={"موبایل"} />
         <CategoryCard image={digitalsCategory} title={"کالای دیجیتال"} />
         <CategoryCard image={homeCategory} title={"لوازم خانگی"} />
@@ -28,6 +29,20 @@ export default function shop() {
         <CategoryCard image={homeCategory} title={"لوازم خانگی"} />
         <CategoryCard image={mobilesCategory} title={"موبایل"} />
         <CategoryCard image={digitalsCategory} title={"کالای دیجیتال"} />
+      </div>
+      <div className="container grid grid-flow-col gap-4 p-2 overflow-scroll my-4">
+        <ProductCard
+          className="!bg-gradient-to-tl from-indigo-300 to-indigo-100"
+          title={
+            <button className="text-blue-600 font-bold border-2 border-solid  border-blue-600 rounded-md w-3/4  sm:w-48 py-2">
+              مشاهده همه
+            </button>
+          }
+        />
+        <ProductCard variant="new" discount={15} price={69700000} />
+        <ProductCard price={39000000} />
+        <ProductCard variant="off" discount={15} price={69700000} />
+        <ProductCard variant="off" discount={15} price={19700000} />
       </div>
     </>
   );
