28bf17b098b1a05f8fdd15de5046d703b2570863
Alireza Salman <itcsalman82@gmail.com>
2024-11-11 23:21:31 -0800

    merge with main

--- epaal/src/app/layout.js
+++ epaal/src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={`${danaFont.className} bg-slate-100`}>
+      <body className={`${danaFont.className} bg-slate-100 max-w-7xl mx-auto`}>
         {children}
       </body>
     </html>
