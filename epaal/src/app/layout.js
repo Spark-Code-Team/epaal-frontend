84ad6cead4bfa696bbc191c905a465ea7ba023fa
Alireza Salman <itcsalman82@gmail.com>
2024-11-11 23:02:53 -0800

    design fonts structure

--- epaal/src/app/layout.js
+++ epaal/src/app/layout.js
@@ -1,29 +1,22 @@
-// font
-import localFont from "next/font/local";
+
 // css
 import "./globals.css";
-import Layout from "@/components/layout/Layout";
+
+// fonts
+import { danaFont } from "@/utils/fonts";
+
 export const metadata = {
   title: "EPAAL",
   description: "Epaal",
 };
 
-const danaFont = localFont({
-  src: [
-    { path: "../../public/fonts/dana.ttf" },
-    { path: "../../public/fonts/dana.woff" },
-    { path: "../../public/fonts/dana.woff2" },
-  ],
-  display: "swap",
-});
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${danaFont.className} bg-slate-100`}>
-        {/* <Layout> */}
         {children}
-        {/* </Layout> */}
       </body>
     </html>
   );
---
+++ epaal/src/utils/fonts.js
@@ -0,0 +1,11 @@
+import localFont from "next/font/local";
+
+
+export const danaFont = localFont({
+    src: [
+        {
+            path: "../../public/fonts/dana.woff2",
+            style: "normal"
+        }
+    ]
+})
