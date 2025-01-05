520fbe0e81697b846c59e81bdc2702c63db7f469
Alireza Salman <itcsalman82@gmail.com>
2025-01-05 11:06:54 -0800

    admin sidebar

--- epaal/src/components/layout/AdminLayout/SidebarAdmin.js
+++ epaal/src/components/layout/AdminLayout/SidebarAdmin.js
@@ -1,10 +1,77 @@
 
-
+const sidebar = [
+    {
+        title: "داشبورد",
+        link: "#"
+    },
+    {
+        title: "دسته بندی محصولات",
+        link: "#"
+    },
+    {
+        title: "فروشندگان",
+        link: "#"
+    },
+    {
+        title: "گزارش ها",
+        link: "#"
+    },
+    {
+        title: "نظرات و بازخورد",
+        link: "#"
+    },
+    {
+        title: "محتوا",
+        link: "#"
+    },
+    {
+        title: "گزارش خطا",
+        link: "#"
+    },
+    {
+        title: "خروج از حساب",
+        link: "#"
+    },
+]
 
 export default function SidebarAdmin() {
 
     return (
-        <>
-        </>
+        <div
+            className="
+                max-h-screen
+                bg-[#33B1AC]
+                overflow-y-scroll
+            "
+        >
+
+            <div
+                className="
+                    bg-[#1F6A67]
+                    h-[86px]
+                "
+            >
+
+            </div>
+
+            {
+                sidebar.map((item, index) => (
+                    <div
+                        className="
+                            h-[86px]
+                            bg-[#33B1AC]
+                        "
+                        key={index}
+                    >
+
+                        <p>
+                            {item.title}
+                        </p>
+
+                    </div>
+                ))
+            }
+
+        </div>
     )
 }
