ec9e1537e281cee13b7d14d6a0f59c4cce4ba85a
Alireza Salman <itcsalman82@gmail.com>
2025-01-12 05:53:14 -0800

    fonts

---
+++ epaal/public/icons/Admin/DoubelArrow.js
@@ -0,0 +1,11 @@
+
+
+
+export default function DoubelArrow() {
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M10.6 17.1998L5 11.5998L10.6 5.99976M19 17.1998L13.4 11.5998L19 5.99976" stroke="#8A8B8D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    )
+}
