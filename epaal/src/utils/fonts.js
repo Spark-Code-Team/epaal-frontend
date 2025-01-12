c9a123c124836f1f4972eaa0f6668e6ae36606ea
Alireza Salman <itcsalman82@gmail.com>
2025-01-12 06:31:26 -0800

    merge

--- epaal/src/utils/fonts.js
+++ epaal/src/utils/fonts.js
@@ -1,43 +1,11 @@
-import localFont from "next/font/local"
+import localFont from "next/font/local";
 
 
-
-export const sparkCodeFont = localFont({
+export const danaFont = localFont({
     src: [
         {
-            path: "../../public/fonts/Dana-Thin.woff2",
-            style: "normal",
-            weight: "100"
-        },
-        {
-            path: "../../public/fonts/Dana-Light.woff2",
-            style: "normal",
-            weight: "300"
-        },
-        {
-            path: "../../public/fonts/Dana-Regular.woff2",
-            style: "normal",
-            weight: "400"
-        },
-        {
-            path: "../../public/fonts/Dana-Medium.woff2",
-            style: "normal",
-            weight: "500"
-        },
-        {
-            path: "../../public/fonts/Dana-SemiBold.woff2",
-            style: "normal",
-            weight: "600"
-        },
-        {
-            path: "../../public/fonts/Dana-Bold.woff2",
-            style: "normal",
-            weight: "700"
-        },
-        {
-            path: "../../public/fonts/Dana-ExtraBold.woff2",
-            style: "normal",
-            weight: "800"
-        },
+            path: "../../public/fonts/dana.woff2",
+            style: "normal"
+        }
     ]
 })
