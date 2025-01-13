9b836cda13719408332f41b2902dff0f9f6a4aa9
omid-develop <omidataee20041382@gmail.com>
2025-01-13 11:31:05 +0330

    component pagination

--- epaal/src/components/module/layout-admin/SellerDetailAdmin.jsx
+++ epaal/src/components/module/layout-admin/SellerDetailAdmin.jsx
@@ -3,6 +3,7 @@ import {IoIosArrowBack, IoIosArrowForward, IoMdSearch} from "react-icons/io";
 import { IoFilterOutline } from "react-icons/io5";
 import { MdKeyboardArrowUp } from "react-icons/md";
 import {useParams} from "next/navigation";
+import Pagination from "../../elements/Pagination";
 
 
 const SellerDetailAdmin = ()=>{
@@ -142,36 +143,7 @@ const SellerDetailAdmin = ()=>{
                 </div>
 
 
-
-
-
-                {/*  pagination  */}
-                <div className="w-full bg-[#E2E2E2] pr-10 pl-10 pt-3 pb-3 mx-auto mt-14 flex mb-3 rounded-md">
-
-                    <div className="w-1/3 mx-auto flex justify-start">
-
-                        <button className="p-1 flex justify-between items-center border-[1px] bg-white border-[#57585A] text-[#57585A] rounded-md">
-                            <IoIosArrowForward/>
-                            صفحه قبلی
-                        </button>
-
-                    </div>
-
-                    <div className="w-1/3 mx-auto flex justify-center items-center text-[#57585A]">
-                        صفحه 1 از 10
-                    </div>
-
-                    <div className="w-1/3 mx-auto flex justify-end">
-
-                        <button className="p-1 text-[#57585A] flex justify-between items-center border-[1px] bg-white border-[#57585A] rounded-md">
-                            صفحه بعدی
-                            <IoIosArrowBack/>
-                        </button>
-
-                    </div>
-
-                </div>
-
+                <Pagination/>
 
 
             </div>
--- epaal/src/components/module/layout-admin/SellersAdmin.jsx
+++ epaal/src/components/module/layout-admin/SellersAdmin.jsx
@@ -2,6 +2,7 @@ import {IoIosArrowBack, IoIosArrowForward, IoMdSearch} from "react-icons/io";
 import {FaPlus} from "react-icons/fa6";
 import Image from "next/image";
 import Link from "next/link";
+import Pagination from "../../elements/Pagination";
 
 const SellersAdmin = ()=>{
 
@@ -90,35 +91,7 @@ const SellersAdmin = ()=>{
                 }
 
 
-
-
-
-                {/*  pagination  */}
-                <div className="w-full bg-[#E2E2E2] pr-10 pl-10 pt-3 pb-3 mx-auto mt-10 flex mb-3 rounded-md">
-
-                    <div className="w-1/3 mx-auto flex justify-start">
-
-                        <button className="p-1 flex justify-between items-center border-[1px] bg-white border-[#57585A] text-[#57585A] rounded-md">
-                            <IoIosArrowForward/>
-                            صفحه قبلی
-                        </button>
-
-                    </div>
-
-                    <div className="w-1/3 mx-auto flex justify-center items-center text-[#57585A]">
-                        صفحه 1 از 10
-                    </div>
-
-                    <div className="w-1/3 mx-auto flex justify-end">
-
-                        <button className="p-1 text-[#57585A] flex justify-between items-center border-[1px] bg-white border-[#57585A] rounded-md">
-                            صفحه بعدی
-                            <IoIosArrowBack/>
-                        </button>
-
-                    </div>
-
-                </div>
+                <Pagination/>
 
 
             </div>
