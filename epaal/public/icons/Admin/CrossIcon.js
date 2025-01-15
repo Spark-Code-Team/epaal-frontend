fe1011c135c6e2d76baa9c3870a4cade5b622b4f
Alireza Salman <itcsalman82@gmail.com>
2025-01-15 05:36:56 -0800

    category

---
+++ epaal/public/icons/Admin/CrossIcon.js
@@ -0,0 +1,11 @@
+
+
+
+export default function CrossIcon() {
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 39 39" fill="none">
+            <path d="M27.495 13.6825L25.1875 11.375L19.435 17.1438L13.6825 11.375L11.375 13.6825L17.1438 19.435L11.375 25.1875L13.6825 27.495L19.435 21.7263L25.1875 27.495L27.495 25.1875L21.7263 19.435L27.495 13.6825Z" fill="black"/>
+        </svg>
+    )
+}
