b33e52fd14a45ce29c788f62986a7aa70feb9833
Mohsen Pourpak <133711640+Mohsen-Pourpak@users.noreply.github.com>
2025-02-18 20:17:14 +0330

    use client added to component

--- src/app/auth-bank/page.js
+++ src/app/auth-bank/page.js
@@ -1,23 +1,13 @@
-import HeadAuth from "@/components/module/auth/HeadAuth"
+import HeadAuth from "@/components/module/auth/HeadAuth";
 import BodyAuthBankPage from "@/components/template/BodyAuthBankPage";
-import { Playwrite_CA } from "next/font/google";
 
-const AuthBank = ()=>{
-
-    return(
-
-        <>
-        
-        
-            <HeadAuth/>
-            <BodyAuthBankPage/>
-
-        
-        </>
-
-    )
-
-}
-
+const AuthBank = () => {
+  return (
+    <>
+      <HeadAuth />
+      <BodyAuthBankPage />
+    </>
+  );
+};
 
 export default AuthBank;
