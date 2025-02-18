7d4fc9566f6afb769016df575c21028f97319e72
Mohsen Pourpak <133711640+Mohsen-Pourpak@users.noreply.github.com>
2025-02-18 12:17:58 +0330

    ci/cd started

--- epaal/src/components/elements/ProductCategories.jsx
+++ epaal/src/components/elements/ProductCategories.jsx
@@ -19,7 +19,7 @@ const ProductCategories = () => {
         <div className="m-auto mt-8 flex w-full flex-wrap justify-around lg:mt-10">
           <div className="bg-gradient-to-t from-evaamCyan rounded-3xl to-gray-100 p-3">
             <Link href="">
-              <Image src={beh} alt={""} width={130} height={130} />
+              <Image src={mobile} alt={""} width={130} height={130} />
               <p className="mt-1 text-center text-base">موبایل</p>
             </Link>
           </div>
@@ -40,28 +40,28 @@ const ProductCategories = () => {
 
           <div className="bg-gradient-to-t from-evaamCyan rounded-3xl to-gray-100 p-3">
             <Link href="">
-              <Image src={sanat} alt={""} width={130} height={130} />
+              <Image src={motor} alt={""} width={130} height={130} />
               <p className="mt-1 text-center text-base">موتور سیکلت</p>
             </Link>
           </div>
 
           <div className="bg-gradient-to-t from-evaamCyan rounded-3xl to-gray-100 p-3">
             <Link href="">
-              <Image src={mobile} alt={""} width={130} height={130} />
+              <Image src={sport} alt={""} width={130} height={130} />
               <p className="mt-1 text-center text-base">لوازم خانگی</p>
             </Link>
           </div>
 
           <div className="bg-gradient-to-t from-evaamCyan rounded-3xl to-gray-100 p-3">
             <Link href="">
-              <Image src={motor} alt={""} width={130} height={130} />
+              <Image src={sanat} alt={""} width={130} height={130} />
               <p className="mt-1 text-center text-base">ابزار صنعتی</p>
             </Link>
           </div>
 
           <div className="bg-gradient-to-t from-evaamCyan rounded-3xl to-gray-100 p-3">
             <Link href="">
-              <Image src={sport} alt={""} width={130} height={130} />
+              <Image src={beh} alt={""} width={130} height={130} />
               <p className="mt-1 text-center text-base">آرایشی بهداشتی</p>
             </Link>
           </div>
