b229d3b59a53c71d06b6f8ccc56a484ea91b4d9f
Alireza Salman <itcsalman82@gmail.com>
2025-02-23 23:53:44 -0800

    secure route

--- src/app/(panel)/dashboard/layout.js
+++ src/app/(panel)/dashboard/layout.js
@@ -1,11 +1,31 @@
+"use client"
+
 import LayoutUser from "@/components/layout/UserPanelLayout/LayoutUser"
+import { fetchRole } from "@/redux/features/userRole/useRole"
+import { redirect } from "next/navigation"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
 
-export default function UserLayout({children}){
+export default function UserLayout({children}) {
+
+    const store = useSelector(store => store)
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        if(!store.role.id) {
+            dispatch(fetchRole())
+        }
+    }, [])
+
+    if(!store.role.id) {
+        redirect("/")
+    }
+
     return (
         <>
             <LayoutUser>
-
                 {children}
             </LayoutUser>
         </>
--- src/components/template/ShopCartDashboardPage.js
+++ src/components/template/ShopCartDashboardPage.js
@@ -10,7 +10,7 @@ import FlashButton from "../elements/FlashButton"
 
 export default function ShopCartDashboardPage() {
 
-    const [state, setSatate] = useState(false)
+    const [state, setSatate] = useState(true)
 
     return (
         <div
@@ -18,54 +18,64 @@ export default function ShopCartDashboardPage() {
                 flex
                 flex-col
                 items-center
-                
             "
         >
-            <Image 
-                src={empety}
-                width={5000}
-                height={5000}
-                className="
-                    w-[244px]
-                    h-[202px]
-                    mt-[149px]
-                    md:mt-0
-                "
-            />
-            <div
-                className="
-                    flex
-                    flex-col
-                    items-center
-                    font-medium
-                "
-            >
-                <p>
-                    سبد خرید شما خالی است!
-                </p>
-                <p
-                    className="
-                        text-[10px]
-                        text-[#8A8B8D]
-                        mt-4
-                    "
-                >
-                    میتوانید برای مشاهده محصولات بیشتر به صفحه فروشگاه بروید:
-                </p>
-            </div>
-            <div
-                className="
-                    w-full
-                    flex
-                    items-center
-                    justify-center
-                "
-            >
-                <FlashButton 
-                    title="صفحه فروشگاه"
-                    href="/shopping-evaam"
-                />
-            </div>
+            {
+                state ? (
+                    <div>
+
+                    </div>
+                ) : (
+                    <>                    
+                        <Image 
+                            src={empety}
+                            width={5000}
+                            height={5000}
+                            className="
+                                w-[244px]
+                                h-[202px]
+                                mt-[149px]
+                                md:mt-0
+                            "
+                            alt="cart"
+                        />
+                        <div
+                            className="
+                                flex
+                                flex-col
+                                items-center
+                                font-medium
+                            "
+                        >
+                            <p>
+                                سبد خرید شما خالی است!
+                            </p>
+                            <p
+                                className="
+                                    text-[10px]
+                                    text-[#8A8B8D]
+                                    mt-4
+                                "
+                            >
+                                میتوانید برای مشاهده محصولات بیشتر به صفحه فروشگاه بروید:
+                            </p>
+                        </div>
+                        <div
+                            className="
+                                w-full
+                                flex
+                                items-center
+                                justify-center
+                            "
+                        >
+                            <FlashButton 
+                                title="صفحه فروشگاه"
+                                href="/shopping-evaam"
+                            />
+                        </div>
+                    </>
+                )
+            }
         </div>
     )
 }
