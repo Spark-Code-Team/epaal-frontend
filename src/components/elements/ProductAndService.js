a271a5b7ab17e6e593b29dbea5b4b63e223bf127
Fereshteakbari <fereshteakbariam18@gmail.com>
2025-05-05 14:15:34 +0330

    header complited

--- src/components/elements/ProductAndService.js
+++ src/components/elements/ProductAndService.js
@@ -6,173 +6,91 @@ import CheckTrueCategory from "../../../public/icons/Admin/CheckTrueCategory";
 import IntroAddProduct from "./IntroAddProduct";
 import { ServiceAndProduct } from "@/service/adminShop";
 
+export default function ProductAndService({
+  setModalState,
+  modalState,
+  setShowModal,
+  selectedIntro,
+  setSelectedIntro,
+}) {
+  const [categories, setCategories] = useState([]);
+  const [isProduct, setIsProduct] = useState(true);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const { response, error } = await ServiceAndProduct(
+        selectedIntro.toplevel_topic.id,
+        isProduct,
+      );
 
-export default function ProductAndService({ setModalState, modalState, setShowModal, selectedIntro, setSelectedIntro }) {
+      if (response) {
+        setCategories(response.data?.data || []);
+      } else {
+        console.log(error);
+      }
+    };
 
-    const [categories, setCategories] = useState([])
-    const [isProduct, setIsProduct] = useState(true)
+    fetchData();
+  }, [isProduct]);
 
-    useEffect(() => {
+  return (
+    <>
+      <div className="h-[300px] w-full overflow-y-scroll">
+        <div className="mt-5 flex items-center gap-2">
+          <p className="text-[12px] text-[#868383]">کالای دیجیتال</p>
+          <LeftAroowBlur width={24} height={24} />
+        </div>
 
-        const fetchData = async () => {
+        <div className="mt-[14px] flex w-full gap-7">
+          <div
+            className="flex cursor-pointer items-center gap-1"
+            onClick={() => setIsProduct(true)}
+          >
+            <p>کالا</p>
+            {isProduct ? <CheckTrueCategory /> : <CheckDefaultCategory />}
+          </div>
+          <div
+            className="flex cursor-pointer items-center gap-1"
+            onClick={() => setIsProduct(false)}
+          >
+            <p>خدمات</p>
+            {isProduct ? <CheckDefaultCategory /> : <CheckTrueCategory />}
+          </div>
+        </div>
 
-            const { response, error } = await ServiceAndProduct(selectedIntro.toplevel_topic.id, isProduct)
+        {categories.length == 0 ? (
+          <div className="flex w-full items-center justify-center">
+            <h1>هیچ دسته بندی یافت نشد</h1>
+          </div>
+        ) : (
+          <div className="mt-[58px] grid max-h-[300px] w-full grid-cols-2 gap-2">
+            {categories.map((items) => (
+              <div
+                key={items}
+                className={`flex w-full cursor-pointer items-center justify-between rounded-xl border p-[18px] ${items.id == selectedIntro.midlevel_topic.id ? "border-blue-600" : null} `}
+                onClick={() =>
+                  setSelectedIntro((last) => ({
+                    ...last,
+                    midlevel_topic: items,
+                  }))
+                }
+              >
+                <p>{items.name}</p>
+                <div>
+                  <AroowLeft />
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
 
-            if(response) {
-                setCategories(response.data?.data || [])
-            } else {
-                console.log(error);
-            }
-        }
-
-        fetchData()
-    }, [isProduct])
-
-    return (
-        <>
-            <div
-                className="
-                    w-full
-                    h-[300px]
-                    overflow-y-scroll
-                "
-            >
-                    <div
-                        className="
-                            flex
-                            items-center
-                            gap-2
-                            mt-5
-                        "
-                    >
-                        <p
-                            className="
-                                text-[#868383]
-                                text-[12px]
-                            "
-                        >
-                            کالای دیجیتال
-                        </p>
-                        <LeftAroowBlur
-                            width={24}
-                            height={24}
-                        />
-                    </div>
-
-                    <div
-                        className="
-                            w-full
-                            flex
-                            gap-7
-                            mt-[14px]
-                        "
-                    >
-                        <div
-                            className="
-                                flex
-                                items-center
-                                gap-1
-                                cursor-pointer
-                            "
-                            onClick={() => setIsProduct(true)}
-                        >
-                            <p>
-                                کالا
-                            </p>
-                            {
-                                isProduct ? (
-                                    <CheckTrueCategory />
-                                ) : (
-                                    <CheckDefaultCategory/>
-                                )
-                            }
-                        </div>
-                        <div
-                            className="
-                                flex
-                                items-center
-                                gap-1
-                                cursor-pointer
-                            "
-                            onClick={() => setIsProduct(false)}
-                        >
-                            <p>
-                                خدمات
-                            </p>
-                            {
-                                isProduct ? (
-                                    <CheckDefaultCategory/>
-                                ) : (
-                                    <CheckTrueCategory />
-                                )
-                            }
-                        </div>
-                    </div>
-
-                    {
-                        categories.length == 0 ? (
-                            <div
-                                className="
-                                    w-full
-                                    flex
-                                    items-center
-                                    justify-center
-                                "
-                            >
-                                <h1>
-                                    هیچ دسته بندی یافت نشد
-                                </h1>
-                            </div>
-                        ) : (
-                            <div
-                                className="
-                                    grid
-                                    grid-cols-2
-                                    gap-2
-                                    w-full
-                                    mt-[58px]
-                                    max-h-[300px]
-                                "
-                        >
-                            {
-                                categories.map(items => (
-                                    <div
-                                        key={items}
-                                        className={`
-                                            w-full
-                                            flex
-                                            items-center
-                                            justify-between
-                                            p-[18px]
-                                            border
-                                            rounded-xl
-                                            cursor-pointer
-                                            ${items.id == selectedIntro.midlevel_topic.id ? "border-blue-600" : null}
-                                        `}
-
-                                        onClick={() => setSelectedIntro(last => ({...last, midlevel_topic: items}))}
-                                    >
-                                        <p>
-                                            {items.name}
-                                        </p>
-                                        <div>
-                                            <AroowLeft />
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                            </div>
-                        )
-                    }
-            </div>
-            
-            <IntroAddProduct 
-                canGo={selectedIntro.midlevel_topic.id &&  categories.length}
-                setModalState={setModalState}
-                setShowModal={setShowModal}
-                modalState={modalState}
-            />
-        </>
-    )
+      <IntroAddProduct
+        canGo={selectedIntro.midlevel_topic.id && categories.length}
+        setModalState={setModalState}
+        setShowModal={setShowModal}
+        modalState={modalState}
+      />
+    </>
+  );
 }
--- src/components/elements/ShopCart.js
+++ src/components/elements/ShopCart.js
@@ -6,29 +6,31 @@ import { useRouter } from "next/navigation";
 import { digitsEnToFa } from "@persian-tools/persian-tools";
 
 export default function ShopCart({ products, icon }) {
-  console.log(" ============== products ==============", products)
   return (
     <Link
       href={`/shopping-evaam/products/${products.product_id}`}
       className="flex w-[350px] flex-col items-center rounded-[10px] border border-[#A7A8A9] px-2 py-[10px] md:w-[372px]"
     >
-
-          <Image
-            src={products.product_image.product_pic ? products.product_image.product_pic : "/"}
-            width={500}
-            height={500}
-            className="h-[150px] w-[150px] md:h-[248px] md:w-[248px]"
-            alt="shop"
-          />
-
-
+      <Image
+        src={
+          products.product_image.product_pic
+            ? products.product_image.product_pic
+            : "/"
+        }
+        width={500}
+        height={500}
+        className="h-[150px] w-[150px] md:h-[248px] md:w-[248px]"
+        alt="shop"
+      />
 
       <div className="w-full text-[10px] font-medium md:text-[16px]">
-        {products.product_name != undefined ? (<>
-            {products.product_name.slice(0, 15) + "..."}
-        </>) : (<>
+        {products.product_name != undefined ? (
+          <>{products.product_name.slice(0, 15) + "..."}</>
+        ) : (
+          <>
             <p> ... </p>
-        </>) }
+          </>
+        )}
       </div>
       <div className="flex w-full items-center justify-between gap-5 text-[10px] font-bold md:text-[16px]">
         <p className="w-/ bg-red-300 text-center"></p>
