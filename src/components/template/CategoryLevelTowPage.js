6eae5a601b57d207dab632e34138cc178077d87b
Alireza Salman <itcsalman82@gmail.com>
2025-05-19 08:35:55 -0700

    admin

--- src/components/template/CategoryLevelTowPage.js
+++ src/components/template/CategoryLevelTowPage.js
@@ -1,9 +1,12 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import CategoryTitleLevel from "../elements/CategoryTitleLevel";
 import PhotoSelect from "../elements/PhotoSelect";
 import { GetAllTopLevelTopic } from "@/service/adminPanel";
+import CrossIcon from "../../../public/icons/Admin/CrossIcon";
+import Image from "next/image";
+import AddPicture from "../../../public/icons/Admin/AddPicture";
 
 
 
@@ -14,6 +17,15 @@ export default function CategorylevelTowPage({ level }) {
         image: "",
         parent: ""
     })
+    const [photo, setPhoto] = useState("")
+    const [img,setImg] = useState("")
+
+    const inputRef = useRef(null)
+    
+    
+    const UploadePhoto =(e) => {
+        setImg(e.target.files[0])
+    }
 
     const [options, setOptions] = useState([])
 
@@ -23,11 +35,9 @@ export default function CategorylevelTowPage({ level }) {
             const { response, error } = await GetAllTopLevelTopic()
 
             if(response) {
-                console.log("888888888888888888888888888888888888888888888888888888",response);
-
                 setOptions(response.data.data)
             } else {
-                console.log("888888888888888888888888888888888888888888888888888888",error);
+                console.log(error);
             }
         }
 
@@ -77,15 +87,110 @@ export default function CategorylevelTowPage({ level }) {
                 "
             />
         </div>
-                <PhotoSelect />
+                {/* <PhotoSelect /> */}
+        <div
+            className="
+                pr-8
+                pt-8
+            "
+        >
+            <p
+                className="
+                    mb-4
+                    text-[20px]
+                    font-medium
+                "
+            >
+                تصویر برای دسته انتخاب کنید
+            </p>
+            <div
+                className="
+                    border-[1px]
+                    border-[#6D6E70]
+                    border-dashed
+                    w-full
+                    h-[157px]
+                    flex
+                    items-center
+                    justify-center
+                "
+            >
+                <div
+                    className="
+                        flex
+                        items-center
+                        justify-center
+                        w-full
+                        h-full
+                        gap-2
+                    "
+                >
+                    {
+                        img ? (
+                            <div
+                                className="
+                                    relative
+                                "
+                            >
+                                <div
+                                    className="
+                                        absolute
+                                        top-[-10px]
+                                        right-[-10px]
+                                        bg-red-600
+                                        rounded-xl
+                                        cursor-pointer
+                                        z-10
+                                    "
+                                    onClick={() => setImg("")}
+                                >
+                                    <CrossIcon />
+                                </div>
+                                <Image
+                                    src={URL.createObjectURL(img)}
+                                    width={300}
+                                    height={300}
+                                    className="
+                                        w-[300px]
+                                        h-full
+                                    "
+                                    alt="alt"
+                                />
+                            </div>
+                        ) : (
+                            <div
+                                onClick={() => inputRef.current.click()}
+                                className="
+                                    cursor-pointer
+                                "
+                            >
+                                <AddPicture />
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+
+            <input 
+                type="file"
+                ref={inputRef}
+                className="
+                    hidden
+                "
+                onChange={(e) => UploadePhoto(e)}
+            />
                 <select
+                title="انتخاب والد"
                     className="
                         w-full
+                        mx-auto
                         pr-8
+                        mt-2
                     "
                 >
                     <option
                         disabled
+                        selected
                     >
                         انتخاب والد
                     </option>
@@ -102,6 +207,7 @@ export default function CategorylevelTowPage({ level }) {
                         ))
                     }
                 </select>
+        </div>
             </div>
             <div
                 className="
